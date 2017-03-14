75bcf76be02e0c7af3c3b75587ac7c27d436c2b8
jenkinNJR <jenkinsraj@hotmail.com>
2017-03-14 13:34:57 +0400

    idea about router

---
+++ cms/app/Http/Controllers/salt.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class salt extends Controller
+{
+    //
+    public function show($id){
+        return "id is ".$id;
+    }
+}
--- cms/routes/web.php
+++ cms/routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return "you are in the login view";
+})->name("login");
+
+Route::get('/register', function () {
+   return view("welcome");
+})->name("register");
+
+Route::get("/foo",function(){
+   return "foo function called ";
+});
+
+Route::get("/foo/{name}/{id}",function($id,$name){
+   return "the detail of foo is ".$id.$name;
+})->where(['id'=>'[0-9]+','name'=>'[a-z]+'])->name("foo");
+
+Route::get("/send",function(){
+   return redirect()->route('register');
+});
+
+Route::get("/salt/{id}","salt@show")->where(['name'=>'salt','id'=>'{0-9}+']);
